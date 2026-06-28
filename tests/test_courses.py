61b387e7103569a1470cd323e1b747453e7266d2
F4nin <tovstoles.aleksei@gmail.com>
2026-06-28 23:02:24 +0300

    PageComponent finish

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,14 +6,14 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.ui
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(courses_page: CoursesListPage):
-    courses_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    courses_page.navbar.check_visible('username')
-    courses_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.sidebar.check_visible()
 
-    courses_page.toolbar_view.check_visible()
-    courses_page.check_visible_empty_view()
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.check_visible_empty_view()
 
 
 @pytest.mark.courses
@@ -25,38 +25,32 @@ def test_empty_courses_list(courses_page: CoursesListPage):
          ("Playwright Advanced", "4 weeks", "Advanced Playwright course", "100", "40"),
      ]
 )
-def test_create_course(courses_page: CoursesListPage, create_course_page: CreateCoursePage, title, estimated_time, description, max_score, min_score):
-    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage, title, estimated_time, description, max_score, min_score):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    create_course_page.create_course_toolbar_view.check_visible()
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_course_page.check_visible_create_course_form(
-        title='',
-        estimated_time='',
-        description='',
-        max_score="0",
-        min_score="0"
+    create_course_page.create_course_form.check_visible(
+        title="", max_score="0", min_score="0", description="", estimated_time=""
     )
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+
+    create_course_page.create_course_exercises_toolbar_view.check_visible()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(
+    create_course_page.create_course_form.fill(
         title=title,
-        estimated_time=estimated_time,
+        max_score=max_score,
+        min_score=min_score,
         description=description,
-        max_score=max_score,
-        min_score=min_score
+        estimated_time=estimated_time
     )
-    create_course_page.click_create_course_button()
-    courses_page.toolbar_view.check_visible()
-    courses_page.course_view.check_visible(
-        index=0,
-        title=title,
-        estimated_time=estimated_time,
-        max_score=max_score,
-        min_score=min_score
+    create_course_page.create_course_toolbar_view.click_create_course_button()
+
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
+        index=0, title=title, max_score=max_score, min_score=min_score, estimated_time=estimated_time
     )
 
 
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -15,8 +15,6 @@ from pages.registration_page import RegistrationPage
 )
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email, username, password) -> None:
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.check_disabled_registration_button()
-    registration_page.fill_registration_form(email=email, username=username, password=password)
-    registration_page.check_visible_registration_button()
+    registration_page.registration_form.fill(email=email, username=username, password=password)
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
